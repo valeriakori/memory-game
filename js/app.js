2acba863243756a525cd60463304364f8e671d87
Mohamad Ghazal <mghazal1024@gmail.com>
2018-04-07 12:08:00 +0200

    feat: javascript main structure undefined
    Broke down the interactive elements of the application into smaller tasks

---
+++ js/app.js
@@ -0,0 +1,60 @@
+/*
+List that holds all cards
+*/
+
+/*
+Display the cards on the page
+  - shuffle the list of cards using the 'shuffle' method
+  - loop through each card and create its html
+  - add each card's HTML to the page
+*/
+
+// Shuffle function
+function shuffle(array) {
+    // Creates a defined variable and 2 undefined variables
+    var currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
+    // While there are elements left on the array...
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+
+      /* Math.floor will round down the number to the previous full integer. ex.. console.log(Math.floor(30.65)); -- returns 30.
+
+      Math.random() -- returns a number between 0 and 1 but never 1
+
+      Therefore,
+      Math.floor(Math.random() * currentIndext);
+      will round down the result from a random number (between 0 and 1) multiply by the current currentIndex value.
+      */
+
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+/*
+Setup the event listener for a card. If a card is clicked:
+  - Display the card's symbol
+  (put this funtionaly in another function that is called from this one)
+
+  - add the card to a *list* of "open" cards
+  (put this funtionaly in another function that is called from this one)
+
+  - if the list already has another card, check to see if the two cards match.
+
+    + if the cards do match, lock the cards in the open position
+    (put this funtionaly in another function that is called from this one)
+
+    + if the cards do not match, remove the cards from the list and hide the card's symbol
+    (put this funtionaly in another function that is called from this one)
+
+    + increment the move counter and display on the page
+    (put this funtionaly in another function that is called from this one)
+
+    + if all cards have match, display a message with the final score
+    (put this funtionaly in another function that is called from this one)
+*/
